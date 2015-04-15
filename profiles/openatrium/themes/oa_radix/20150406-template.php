8b66c97de9d469ce0773c87be7219a8550830302
Maxias Dev <ubuntu@ip-10-135-45-12.ap-southeast-1.compute.internal>
2015-04-15 03:34:46 +0000

    Initial commit

---
+++ profiles/openatrium/themes/oa_radix/20150406-template.php
@@ -0,0 +1,138 @@
+	<?php
+
+/**
+ * @file
+ * Theme functions
+ */
+
+require_once dirname(__FILE__) . '/includes/structure.inc';
+require_once dirname(__FILE__) . '/includes/form.inc';
+require_once dirname(__FILE__) . '/includes/menu.inc';
+require_once dirname(__FILE__) . '/includes/comment.inc';
+require_once dirname(__FILE__) . '/includes/node.inc';
+
+/**
+ * Implements hook_css_alter().
+ * Changes the jQuery UI theme to a Bootstrap-like theme
+ * from http://addyosmani.github.io/jquery-ui-bootstrap/
+ */
+function oa_radix_css_alter(&$css) {
+  $radix_path = drupal_get_path('theme', 'radix');
+
+  // Radix now includes compiled stylesheets for demo purposes.
+  // We remove these from our subtheme since they are already included
+  // in compass_radix.
+  unset($css[$radix_path . '/assets/stylesheets/radix-style.css']);
+  unset($css[$radix_path . '/assets/stylesheets/radix-print.css']);
+
+  // Add a custom jQuery UI theme.
+  if (isset($css['misc/ui/jquery.ui.theme.css'])) {
+    $css['misc/ui/jquery.ui.theme.css']['data'] =
+      drupal_get_path('theme', 'oa_radix') . '/assets/vendor/jqueryui/jquery-ui-1.10.0.custom.css';
+  }
+}
+
+/**
+ * Implements hook_module_implements_alter().
+ * Remove panopoly_core which uses this alter to set it's own jquery_ui theme
+ * Should be done in a theme layer, not in panopoly_core
+ */
+function oa_radix_module_implements_alter(&$implementations, $hook) {
+  if ($hook == 'element_info_alter') {
+    unset($implementations['panopoly_core']);
+  }
+}
+
+/**
+ * Implements template_preprocess_page().
+ */
+function oa_radix_preprocess_page(&$vars) {
+  // Rework search_form to our liking.
+  $vars['search_form'] = '';
+  if (module_exists('search') && user_access('search content')) {
+    $search_box_form = drupal_get_form('search_form');
+    $search_box_form['basic']['keys']['#title'] = '';
+    $search_box_form['basic']['keys']['#attributes'] = array('placeholder' => 'Search');
+    $search_box_form['basic']['keys']['#attributes']['class'][] = 'search-query';
+    $search_box_form['basic']['submit']['#value'] = t('Search');
+    $search_box_form['#attributes']['class'][] = 'navbar-form';
+    $search_box_form['#attributes']['class'][] = 'pull-right';
+    $search_box = drupal_render($search_box_form);
+    $vars['search_form'] = (user_access('search content')) ? $search_box : NULL;
+  }
+
+  // Add user_badge to header.
+  $vars['user_badge'] = '';
+  if (module_exists('oa_dashboard')) {
+    $user_badge = module_invoke('oa_dashboard', 'block_view', 'oa_user_badge');
+    $vars['user_badge'] = $user_badge['content'];
+  }
+  $toolbar = panels_mini_block_view('oa_toolbar_panel');
+  $vars['oa_toolbar_panel'] = isset($toolbar) ? $toolbar['content'] : '';
+  $footer = panels_mini_block_view('oa_footer_panel');
+  $vars['oa_footer_panel'] = isset($footer) ? $footer['content'] : '';
+
+  ctools_include('content');
+  $banner = ctools_content_render('oa_space_banner', '', array(
+    'banner_position' => 2
+  ));
+  if (!empty($banner->content)) {
+    $vars['oa_banner'] = $banner->content;
+  }
+  $vars['oa_space_menu'] = '';
+  $space_id = oa_core_get_space_context();
+  if (variable_get('oa_space_menu_' . $space_id, TRUE)) {
+    $space_menu = ctools_content_render('oa_space_menu', '', array(), array());
+    if (!empty($space_menu->content)) {
+      $vars['oa_space_menu'] = $space_menu->content;
+    }
+  }
+  // Set Breadrumb for LMS pages based on alias.
+  $lms  = strpos(drupal_get_path_alias(current_path()),'lms/');
+  if($lms !== false){
+
+    $breadcrumb = array();
+    $breadcrumb[] = l('Home', '<front>');
+    $breadcrumb[] = l('LMS', 'courses-list');
+
+    drupal_set_breadcrumb($breadcrumb);
+  }
+  // Set breadcrumb for CRM pages based on alias.
+  $crm  = strpos(drupal_get_path_alias(current_path()),'crm/');
+  if($crm !== false){
+
+    $breadcrumb = array();
+    $breadcrumb[] = l('Home', '<front>');
+    $breadcrumb[] = l('CRM', 'spaces');
+
+    drupal_set_breadcrumb($breadcrumb);
+  }
+}
+
+function oa_radix_breadcrumb($variables) {
+  $breadcrumb = $variables['breadcrumb'];
+  $breadcrumb = array_unique($breadcrumb);
+    
+  if (!empty($breadcrumb)) {
+    // Provide a navigational heading to give context for breadcrumb links to
+    // screen-reader users. Make the heading invisible with .element-invisible.
+    $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+    $crumbs = '<div class="breadcrumb">';
+
+    $array_size = count($breadcrumb);
+    $i = 0;
+    while ( $i < $array_size) {
+    
+      $pos = strpos($breadcrumb[$i], drupal_get_title());
+      //we stop duplicates entering where there is a sub nav based on page jumps
+      if ($pos === false){
+        $crumbs .= '<span class="breadcrumb-' . $i;
+        $crumbs .=  '"&raquo;' . $breadcrumb[$i] . '</span> &gt; ';
+      }
+      $i++;
+    }
+    $crumbs .= '<span class="active">'. drupal_get_title() .'</span></div>';
+    return $crumbs;
+  }
+}
+?>
---
+++ profiles/openatrium/themes/oa_radix/assets/javascripts/script.js
@@ -0,0 +1,266 @@
+(function ($) {
+  $(document).ready(function() {  	
+  	//Add custom text on quiz results
+	var score = $("#quiz_score_percent em.placeholder").text();
+	
+	if(score < 75){
+		$( "<p id='messageText'>You need 75% to complete this quiz. Please try again.<a href='http://stagefms.mx.sg/?q=content/quiz'>Click Here</a></p>" ).insertAfter( "#quiz_summary" );
+	}
+	
+	if(score >= 75){
+		$( "<p id='messageText'>Congratulations you have completed the quiz! This product is now avaliable in your profile.</p>" ).insertAfter( "#quiz_summary" );
+	}
+	
+	
+  	//ADD ACTIVITY POPUP ERROR MESSAGE
+	$("form#comment-form").submit(function(){
+		var errorMsg = [];
+		var errorText = "";
+		
+		if($("#edit-field-account-und").val() == 1){
+			if($("#edit-field-report-status-und").val() == "_none" || $("#edit-field-report-status-und").val() == null){
+				errorMsg.push("Activity status field is required!");
+			}
+			
+			/*if($("#edit-field-account-und").val() == ""){
+				errorMsg.push("Account field is required!");
+			}
+			
+			if($(".date-clear").val() == ""){
+				errorMsg.push("Appointment date and time is required!");
+			}*/
+			
+			if($("#edit-comment-body-und-0-value").val() == ""){
+				errorMsg.push("Remarks field is required!");
+			}
+		}
+		else{
+			if($("#edit-field-report-status-und").val() == "_none" || $("#edit-field-report-status-und").val() == ""){
+				errorMsg.push("Activity status field is required!");
+			}
+			
+	/*		if($("#edit-field-account-und").val() == ""){
+				errorMsg.push("Account field is required!");
+			}
+			
+			if($(".date-clear").val() == ""){
+				errorMsg.push("Appointment date and time is required!");
+			}*/
+			
+			if($("#edit-comment-body-und-0-value").val() == ""){
+				errorMsg.push("Remarks field is required!");
+			}
+			/*
+			if($("#edit-field-cheque-no-und-0-value").val() == ""){
+				errorMsg.push("Cheque No field is required!");
+			}
+			
+			if($("#edit-field-bank-name-und-0-value").val() == ""){
+				errorMsg.push("Bank Name field is required!");
+			}
+			
+			if($("#edit-field-cheque-amount-und-0-value").val() == ""){
+				errorMsg.push("Amount field is required!");
+			}
+			
+			if($("#edit-field-product-und").val() == "_none" || $("#edit-field-product-und").val() == ""){
+				errorMsg.push("Product Type field is required!");
+			}
+			
+			if($("#edit-field-product-amount-und-0-value").val() == ""){
+				errorMsg.push("Product Amount field is required!");
+			}
+			
+			if($("#edit-field-attachments-und-0-upload").val() == ""){
+				errorMsg.push("Please choose a file to upload!");
+			}*/
+		}
+		
+		if(errorMsg != ""){
+			$.each( errorMsg, function(key, value ) {
+			 errorText += value+"\n";
+			});
+			alert(errorText);
+			return false; 
+		}else{
+			$("form#comment-form").submit();
+			return true;
+		}
+	});
+	
+	
+    // Handle toolbar collapse.
+    $('.oa-navigation .btn-navbar-menu').click(function(e) {
+      e.preventDefault();
+      var navMenuCollapse = $('.oa-navigation .nav-menu-collapse');
+      var height = (navMenuCollapse.height() == 0) ? 'auto' : 0;
+      navMenuCollapse.height(height);
+    });
+    
+    $('body.page-node-add-quiz #taking-fieldset div.form-type-checkbox input').change(function(){
+       if (this.checked){
+          $(this).addClass('checked');
+          $(this).parent('div.form-type-checkbox').addClass('checked');
+       } else {
+          $(this).removeClass('checked');
+          $(this).parent('div.form-type-checkbox').removeClass('checked');
+       }
+    });
+    
+    
+    $('.page-courses-list .view-courses-list .views-row').each(function(){
+        var value = $(this).find('.views-field-complete span.field-content').text();
+        
+        if(value == '1')
+        {
+            $(this).find('.views-field-nothing span a').text('Completed');
+            $(this).find('.views-field-nothing').addClass('completed');
+            $(this).find('.views-field-title span').addClass('completed');
+        }
+        else
+        {
+            $(this).find('.views-field-nothing span a').text('View course');
+            $(this).find('.views-field-nothing').removeAttr('completed');
+            $(this).find('.views-field-title span').removeAttr('completed');
+        }
+    });
+    
+    //Login fields placeholder
+    $('#user-login-panel div.form-item input#edit-name').attr('placeholder','Username');
+    $('#user-login-panel div.form-item input#edit-pass').attr('placeholder','Password');
+    
+    
+    // adjust the position of the IPE buttons
+    var $ipe = $('#panels-ipe-control-container');
+    if ($ipe.length) {
+      var $main = $('#main');
+      var $element = $ipe.detach();
+      $main.prepend($element);
+    }
+	
+	// change pane title on report page
+	$('.node-type-oa-space .bryant-flipped-sidebar h3.pane-title').html("SALES TEAM");
+	$('.node-type-customer .moscone-flipped-sidebar-inner ul.links.inline li a').html("ADD ACTIVITY");
+	$('.node-type-customer .pane-node-comment-form .field-name-comment-body label').html("REMARKS");
+	
+	$('.node-type-customer .moscone-flipped-sidebar-inner .pane-entity-field .field-label').each(function(){
+		$(this).html("");
+	});
+	
+	$('.node-type-customer .moscone-flipped-sidebar-inner .pane-entity-field .field-name-field-company-address .field-label').append("<img class='icon_img' src='/profiles/openatrium/themes/oa_radix/assets/images/icon_img/icon_address.png'/>");
+	
+	$('.node-type-customer .moscone-flipped-sidebar-inner .pane-entity-field .field-name-field-contacts .entity-field-collection-item .field-name-field-con-cus-name .field-label').append("<img class='icon_img' src='/profiles/openatrium/themes/oa_radix/assets/images/icon_img/icon_contacts.png'/>");
+
+	$('.node-type-customer .moscone-flipped-sidebar-inner .pane-entity-field .field-name-field-contacts .entity-field-collection-item .field-label').each(function(){	
+		$(this).append("<img class='icon_img1' src='/profiles/openatrium/themes/oa_radix/assets/images/icon_img/icon_space.png'/>");
+	});
+	// end of script for pane title on http://fms.mx.sg/jing-ding-technologies-pte-ltd side bar title
+	
+	// change OPEN/LOCK color in contact report page
+	if($(".node-type-customer .moscone-flipped-sidebar-inner .pane-node-field-cus-status .field-name-field-cus-status .field-item").text() == "Lock"){
+		$(".node-type-customer .moscone-flipped-sidebar-inner .pane-node-field-cus-status .field-name-field-cus-status .field-item").addClass("lock");
+	}else{
+		$(".node-type-customer .moscone-flipped-sidebar-inner .pane-node-field-cus-status .field-name-field-cus-status .field-item").addClass("open");
+	}
+	// end of script for OPEN/LOCK color in contact report page
+	
+	jQuery.fn.center = function ()
+	{
+	    this.css("position","fixed");
+	    this.css("left", ($(window).width() / 2) - (this.outerWidth() / 2));
+	    return this;
+	}
+
+	// script for popup report page 
+	$('.node-type-customer .pane-node-comment-form').center();
+	$(window).resize(function(){
+	   $('.node-type-customer .pane-node-comment-form').center();
+	});
+	
+	$(".node-type-customer .pane-node-comment-form").css({
+		"background":"#FFFFFF",
+		"display":"none",
+		"position":"fixed",
+		"top":"2%",
+		"z-index":"9999999999999999999",
+		"padding":"20px",
+		"width":"60%",
+		"height":"90%",
+		"overflow-y":"scroll"
+	});
+	
+	$(".node-type-customer div#cboxOverlay").css({
+		"display":"none",
+		"position":"fixed",
+		"width":"100%",
+		"height":"100%",
+		"background":"#000",
+		"opacity":".5",
+		"z-index":"999999999",
+		"top":"0"
+	});
+
+    $('.node-type-customer .moscone-flipped-sidebar-inner ul.links.inline li a').click(function(){
+	    $('div.panel-pane.pane-node-comment-form').show();
+        $('div#cboxOverlay').show();
+    });
+
+    $('div#cboxOverlay').click(function(){
+        $('div.panel-pane.pane-node-comment-form').hide();
+        $('div#cboxOverlay').hide();
+    });
+	
+	$(".node-type-customer .moscone-flipped-sidebar-inner ul.links.inline li a").click(function(){return false});
+	// end of script for popup report page 
+	
+	//change || add || insert text		
+	$(".node-type-customer .pane-node-comment-form .comment-form #edit-actions input").attr("value","SAVE ACTIVITY");	
+	$(".node-type-customer .pane-node-comment-form .comment-form .form-wrapper .form-item-field-report-status-und label").html("ADD ACTIVITY");	
+	$(".node-type-customer .pane-node-comment-form .comment-form .form-wrapper .form-item-field-account-und label").html("");	
+	$("<div id='addreport'>ADD ACTIVITY</div>").insertBefore(".node-type-customer .pane-node-comment-form .pane-content form");	
+	
+	$(".node-type-customer .pane-node-comment-form #edit-field-product-und option:first-child").html("Product Type");
+	$(".node-type-customer .pane-node-comment-form #edit-field-product-und option:first-child").css({"color":"#808080"});
+	var text = "Business Reg No.&nbsp;";
+	$(".node-type-customer .moscone-flipped-column-content-region-inner .pane-node-field-brn .field-label").html(text);
+	$(".node-type-customer .moscone-flipped-column-content-region-inner .pane-node-comments h3.pane-title").html("ACTIVITY FIELD");
+	$('.node-type-customer .pane-node-comment-form #edit-field-report-status-und option:contains("- Select a value -")').text('Select Status *');
+	$('.node-type-customer .pane-node-comment-form #edit-field-attachments-und-0-upload').text('Attachment');
+	$('.node-type-customer .pane-node-comment-form #edit-field-report-status-und').css("color","#808080");
+	$('.node-type-customer .pane-node-comment-form #edit-field-account-und').css("color","#808080");
+	$('.node-type-customer .pane-node-comment-form #edit-field-product-und').css("color","#808080");
+	$('.node-type-customer .pane-node-comment-form #edit-field-product-amount-und-0-value').css("color","#808080");
+	$('.node-type-customer .pane-node-comment-form #edit-comment-body-und-0-value').css("color","#808080");
+	//$('.node-type-course h1.title').html("Website Marketing");
+	$('.node-type-course .brenham-flipped-column-content-region-inner .panel-pane h3').html("LESSON PLAN");
+	$('.node-type-quiz h1.title').html("Website Marketing Quiz");
+	
+	//add label to stylize the radio boxes in page website marketing quiz
+	$(".node-type-quiz .region-content form .form-item-tries-answer .form-radio").each(function(){
+		$(this).after("<label></label>");
+	});
+	
+	var ficon = $(".node-type-course .brenham-flipped-column-content-region.span9 .pane-course-outline .item-list ul li.ppt-slides-course-content a").length;
+	var sicon = $(".node-type-course .brenham-flipped-column-content-region.span9 .pane-course-outline .item-list ul li.course-documents-course-content a").length;
+	var ticon = $(".node-type-course .brenham-flipped-column-content-region.span9 .pane-course-outline .item-list ul li.video-materials-course-content a").length;
+	
+	if(ficon != 0){
+		$(".node-type-course .brenham-flipped-column-content-region.span9 .pane-course-outline .item-list ul li.ppt-slides-course-content a").html("<img class='show_icon' src='/profiles/openatrium/themes/oa_radix/assets/images/icon_img/view_slides.png' alt='' title='' />");
+	}else{
+		$(".node-type-course .brenham-flipped-column-content-region.span9 .pane-course-outline .item-list ul li.ppt-slides-course-content").html("<img class='show_icon' src='/profiles/openatrium/themes/oa_radix/assets/images/icon_img/view_slides.png' alt='' title='' />");
+	}
+	
+	if(sicon != 0){
+		$(".node-type-course .brenham-flipped-column-content-region.span9 .pane-course-outline .item-list ul li.course-documents-course-content a").html("<img class='show_icon' src='/profiles/openatrium/themes/oa_radix/assets/images/icon_img/view_documents.png' alt='' title='' />");
+	}else{
+		$(".node-type-course .brenham-flipped-column-content-region.span9 .pane-course-outline .item-list ul li.course-documents-course-content").html("<img class='show_icon' src='/profiles/openatrium/themes/oa_radix/assets/images/icon_img/view_documents.png' alt='' title='' />");
+	}
+	
+	if(ticon != 0){
+		$(".node-type-course .brenham-flipped-column-content-region.span9 .pane-course-outline .item-list ul li.video-materials-course-content a").html("<img class='show_icon' src='/profiles/openatrium/themes/oa_radix/assets/images/icon_img/view_videos.png' alt='' title='' />");
+	}else{
+		$(".node-type-course .brenham-flipped-column-content-region.span9 .pane-course-outline .item-list ul li.video-materials-course-content").html("<img class='show_icon' src='/profiles/openatrium/themes/oa_radix/assets/images/icon_img/view_videos.png' alt='' title='' />");
+	}
+	
+  });
+})(jQuery)
